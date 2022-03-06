61b5119a791d3fa9c07e328debe8c660b03936d4
Atharva Joshi <atharvamj270800@gmail.com>
2022-03-06 10:12:38 +0530

    doctor upload documents

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,50 +1,50 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, AbstractUser
+from django.db import connection, models
+from django.conf import settings
+import os
 
-
-from django.db import models
 
 # Create your models here.
 class CustomUserManager(BaseUserManager):
-    def create_user(self, name, email, password=None, is_admin=False):
+    def create_user(self, name, email, user_type, password=None, phone=None, is_admin=False):
         if not name:
             raise ValueError("Please provide name")
         if not email:
             raise ValueError("Please provide email")
         if not password:
             raise ValueError("Please provide password")
+        if not phone:
+            raise ValueError("Please provide phone")
+        if not user_type in ["0", "1"]:
+            raise ValueError("Please provide correct user type")
 
         user = self.model(email=self.normalize_email(email))
         user.name = name
+        user.user_type = user_type
         user.is_admin = is_admin
         user.set_password(password)
+        user.phone = phone
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, name, email, password):
-        return self.create_user(name=name, email=email, password=password, is_admin=True)
+    def create_superuser(self, name, email, password, phone, user_type="0"):
+        return self.create_user(name=name, email=email, password=password, phone=phone, user_type=user_type, is_admin=True)
 
 
 class User(AbstractBaseUser):
     USERNAME_FIELD = 'email'
-    user_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=256)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=17, blank=True)
     password = 	models.CharField(max_length=255)
-    user_type = models.SmallIntegerField() # doctor or patient or admin
-
+    user_type = models.CharField(max_length=1, default="0") # doctor or patient or admin
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
-    # optional fields
-    # profile_pic = models.ImageField(default='Profiles/default.png')
-    # about = models.TextField(null=True, blank=True)
-    # twitter = models.URLField(null=True, blank=True)
-    # linkedin = models.URLField(null=True, blank=True)
-
-
-
+    
     REQUIRED_FIELDS = ['name', 'password', 'phone']
 
     objects = CustomUserManager() # Note this
@@ -66,4 +66,84 @@ class User(AbstractBaseUser):
     
     # @property
     def has_module_perms(self, package_name=None):
-        return self.is_admin and self.is_act
+        return self.is_admin and self.is_active
+
+# class User(AbstractUser):
+#     name = models.CharField(max_length=20)
+
+class Patient(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    encryption_key = models.CharField(max_length=512)
+
+
+class Doctor(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    hospital = models.CharField(max_length=256, blank=True, null=True)
+    degree = models.CharField(max_length=256, blank=True, null=True)
+    verified = models.BooleanField(default=False)
+
+    
+
+class Document(models.Model):
+    id = models.AutoField(primary_key=True)
+    document = models.FileField()
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    def save(self, *args, **kwargs):
+        super(Document, self).save(*args, **kwargs)
+        # print(f"DEFAULT URL : {self.profile_pic.url}")
+        BASE_DIR = settings.BASE_DIR
+        UPLOAD_TO = f'Documents/{self.id}'
+        path = os.path.join(BASE_DIR, f'media/Documents/{self.id}')
+        if not os.path.exists(path):
+            os.makedirs(path)
+        # print(f"profile_pic.name = {self.profile_pic.name}")
+        # print(f"profile pic name = {self.profile_pic.name}")
+        filename = self.document.name
+        extension = filename.split('.')[-1]
+
+        new_name = f"{filename}_{self.id}.{extension}"
+        location = os.path.join(BASE_DIR, os.path.join('media'))
+        print(f"Previous location: {os.path.join(location, filename)}, New location: {os.path.join(location, os.path.join(UPLOAD_TO, new_name))}")
+        os.replace(os.path.join(location, filename), os.path.join(location, os.path.join(UPLOAD_TO, new_name)))
+        self.document.name = os.path.join(UPLOAD_TO, new_name)
+        print(f"name = ", self.document.name)
+        super(Document, self).save(*args, **kwargs)
+
+
+
+class Report(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=256)
+    date = models.DateField()
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+
+
+class Xray(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=256)
+    image = models.ImageField()
+    report = models.ForeignKey(Report, on_delete=models.CASCADE)
+
+
+class Mri(models.Model):
+    id = models.AutoField(primary_key=True)
+    image = models.ImageField()
+    title = models.CharField(max_length=256)
+    report = models.ForeignKey(Report, on_delete=models.CASCADE)
+
+
+class Prescription(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=256)
+    prescription = models.FileField()
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+
+    # optional fields
+    # profile_pic = models.ImageField(default='Profiles/default.png')
+    # about = models.TextField(null=True, blank=True)
+    # twitter = models.URLField(null=True, blank=True)
+    # linkedin = models.URLField(null=True, blank=True)
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,19 +1,56 @@
-from django.shortcuts import render
-
+import datetime
+from django.db import connection
+from django.shortcuts import HttpResponse, redirect, render, HttpResponseRedirect, reverse
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import views
+from rest_framework.response import Response
+from .models import *
+from .forms import CreateUser
+import string
+import random
 # Create your views here.
+@login_required
+def index(request):
+    user = request.user
+    if user.user_type == 0: # patient
+        return redirect('index-patient')
+    else: # practioner
+        return redirect('index-doctor')
+
+@login_required
+def index_doctor(request):
+    user = request.user
+    if user.user_type == "0":
+        return redirect('index-patient')
+    doctor = user.doctor
+    if not doctor.verified:
+        return redirect('upload-docs')
+    
+    return HttpResponse("Work in progress")
+
+@login_required
+def index_patient(request):
+    user = request.user
+    if user.user_type == "1":
+        return redirect('index-doctor')
+
 def register(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect(reverse('index'))
-        
-    if request.method == "POST":	
-        name = request.POST['name']
-        email = request.POST['email']
-        phone = request.POST['phone']
-        password = request.POST['password']
-        user_type = request.POST['user_type']
 
+    if request.method == "POST":	
+        form = CreateUser(request.POST)
         if form.is_valid():
             instance = User.objects.create_user(**form.cleaned_data)
+            if instance.user_type == "0":
+                patient = Patient(user=instance, encryption_key="".join(random.choices(string.ascii_letters+string.digits, k=512)))
+                patient.save()
+            else:
+                doctor = Doctor(user=instance)
+                doctor.save()
             return HttpResponseRedirect(reverse('login')) #HttpResponse("<b>User Created Successfully</b>")
 
         else:
@@ -21,7 +58,57 @@ def register(request):
             context = {
                 "error" : "Email alreday exists.",
             }
-            return render(request, 'backend/register.html', context=context)
-            # return render(request, 'General/amj_register.html', context)
+            return render(request, 'app/register.html', context=context)
     else:
-        return render(request, 'backend/register.html')
+        return render(request, 'app/register.html')
+
+
+def log_in(request):
+    user = request.user
+    if user.is_authenticated:
+        return redirect('index')
+
+    if request.method == "GET":
+        return render(request, 'app/login.html')
+    
+    if request.method == "POST":
+        print(f"post params: {request.POST}")
+        user = authenticate(request, email=request.POST["email"], password=request.POST["password"])
+        if not user is None:
+            login(request, user)
+            return redirect('index')
+        context = {
+            'error' : 'Please provide correct email and password'
+        }
+        return render(request, 'app/login.html', context)
+    return redirect(request, 'index')
+
+
+@login_required
+def log_out(request):
+    logout(request)
+    return redirect('index')
+
+
+@login_required
+@require_http_methods(["GET", "POST"])
+def upload_docs(request):
+    if request.user.user_type == "0":
+        return redirect(request, 'index')
+    doctor = request.user.doctor
+    if request.method == "GET":
+        context = {"hospital": doctor.hospital, "degree": doctor.degree}
+        return render(request, 'app/upload_docs.html', context=context)
+
+    if request.method == "POST":
+        d = Document()
+        d.document = request.FILES["document"]
+        d.doctor = doctor
+        hospital = request.POST["hospital"]
+        degree = request.POST["degree"]
+        doctor.hospital = hospital
+        doctor.degree = degree
+        doctor.save()
+        d.save()
+    return redirect('index')
+    
